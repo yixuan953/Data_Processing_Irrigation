d81b785a5d5fbfab44aa18800ca27a92d1bbf71b
yixuan953 <zhou111@master200.internal.anunna.wur.nl>
2025-04-09 14:45:30 +0200

    Add Distribution to main crop

---
+++ 2_Irrigation_Distribution.bash
@@ -0,0 +1,78 @@
+#!/bin/bash
+#-----------------------------Mail address-----------------------------
+
+#-----------------------------Output files-----------------------------
+#SBATCH --output=HPCReport/output_%j.txt
+#SBATCH --error=HPCReport/error_output_%j.txt
+
+#-----------------------------Required resources-----------------------
+#SBATCH --time=600
+#SBATCH --mem=250000
+
+#--------------------Environment, Operations and Job steps-------------
+# module load python/3.12.0
+
+# Step 1: Calculate how much irrigation water (fraction) would go to main crop
+# 1-1: Transform the data from tiff format to nc (SPAM2005)
+# python /lustre/nobackup/WUR/ESG/zhou111/Code/Data_Processing/Irrigation/1_1_Trans_tiff_nc.py
+# 1-2: Calculate the fraction of main crop 
+# python /lustre/nobackup/WUR/ESG/zhou111/Code/Data_Processing/Irrigation/1_2_Cal_Frac.py
+
+# Step 2: Calculate the amount total irrigation water goes to the main crops
+module load cdo
+IRRIG_FILE="/lustre/nobackup/WUR/ESG/zhou111/Data/Raw/Irrigation/VIC_Bram/irrigationWithdrawal_monthly_1979_2016.nc"
+FRAC_FILE="/lustre/nobackup/WUR/ESG/zhou111/Data/Irrigation/MainCrop_Fraction_05d.nc"
+process_dir="/lustre/nobackup/WUR/ESG/zhou111/Data/Processed/Irrigation"
+output_dir="/lustre/nobackup/WUR/ESG/zhou111/Data/Irrigation"
+
+# 2-1: Sum up the total irrigation amount from all sectors (e.g., groundwater, reservoir, etc.) # Unit: [mm]
+Sum_Irri(){
+    echo "Summing all irrigation withdrawal sectors..."
+    cdo -O enssum -select,name=OUT_WI_COMP_SECT,OUT_WI_DAM_SECT,OUT_WI_GW_SECT,OUT_WI_NREN_SECT,OUT_WI_REM_SECT,OUT_WI_SURF_SECT $IRRIG_FILE $process_dir/total_irrigation_tmp.nc
+    cdo -O chname,OUT_WI_COMP_SECT,TOTAL_IRRIGATION $process_dir/total_irrigation_tmp.nc $process_dir/total_irrigation.nc
+    rm $process_dir/total_irrigation_tmp.nc
+}
+
+# Sum_Irri
+
+# 2-2: Match the grid of two .nc files if needed(total irrigation amount & fraction goes to main crop)
+Grid_Match(){
+    echo "Checking grid definitions..."
+    IRRIG_GRID=$(cdo griddes $process_dir/total_irrigation.nc)
+    FRAC_GRID=$(cdo griddes $FRAC_FILE)
+
+    # Compare grid information (number of cells should be sufficient)
+    IRRIG_GRID_SIZE=$(echo "$IRRIG_GRID" | grep "gridsize" | awk '{print $3}')
+    FRAC_GRID_SIZE=$(echo "$FRAC_GRID" | grep "gridsize" | awk '{print $3}')
+
+    if [ "$IRRIG_GRID_SIZE" != "$FRAC_GRID_SIZE" ]; then
+        echo "Grids don't match. Regridding irrigation data to match fraction data..."
+        # Create a weights file for conservative remapping
+        cdo gencon,${FRAC_FILE} $process_dir/total_irrigation.nc $process_dir/weights.nc
+        # Apply the weights for remapping
+        cdo -O remap,${FRAC_FILE},$process_dir/weights.nc $process_dir/total_irrigation.nc $process_dir/total_irrigation_regridded.nc
+        TOTAL_IRRIG_FILE="$process_dir/total_irrigation_regridded.nc"
+    else
+        echo "Grids match. No regridding needed."
+        TOTAL_IRRIG_FILE="$process_dir/total_irrigation.nc"
+    fi
+}
+
+# Grid_Match
+
+# 2-3 Multiply total irrigation by main crop fraction
+Get_Irri_MainCrop(){
+    echo "Calculating irrigation for main crops..."
+    cdo selvar,Frac_MainCrop $output_dir/MainCrop_Fraction_05d.nc $process_dir/frac_maincrop.nc # Unit [-]
+    cdo selvar,TOTAL_IRRIGATION $output_dir/total_irrigation.nc $process_dir/total_irrigation_only.nc # Unit []
+    cdo invertlat $process_dir/total_irrigation_only.nc $process_dir/total_irrigation_only_latinvert.nc
+    cdo selvar,area /lustre/nobackup/WUR/ESG/zhou111/Data/Raw/General/pixel_area_m2_05d.nc $process_dir/pixel_area_05d.nc
+    cdo invertlat $process_dir/pixel_area_05d.nc $process_dir/pixel_area_05d_latinvert.nc
+    cdo merge $process_dir/total_irrigation_only_latinvert.nc $process_dir/frac_maincrop.nc $process_dir/pixel_area_05d_latinvert.nc $process_dir/merged_input.nc
+    cdo -L -expr,'MAIN_CROP_IRRIGATION=TOTAL_IRRIGATION*Frac_MainCrop*area/1000' \
+        -merge $process_dir/merged_input.nc \
+        $output_dir/maincrop_irrigation.nc
+
+}
+
+Get_Irri_MainCrop
--- Readme.txt
+++ Readme.txt
@@ -1,9 +1,20 @@
-The scripts of this folder are used to distribute the monthly irrigation amount from Droppers et al. (2020): https://doi.org/10.5194/gmd-13-5029-2020 to main crop types
+The scripts of this folder are used to distribute the monthly irrigation amount (1980 - 2016) from Droppers et al. (2020): https://doi.org/10.5194/gmd-13-5029-2020 to main crop types
 The distribution methods are as follows:
 
-1 - Calculate how much irrigation water will go to the main crops (maize, rice, wheat, soybean):
+1 - Calculate how much irrigation water (fraction - [0,1]) will go to the main crops (maize, rice, wheat, soybean):
     Frac_MainCrop = (Havest area * Irrigated proportion)_MainCrop/(Havest area * Irrigated proportion)_AllCrop
     Here we used the SPAM2005 data (an example of rice) (https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/DHXBJX):
         - Havest area [ha]: SPAM2005V3r2_global_H_TA_RICE_A.tif 
         - Irrigation proportion [-, 0-1]: SPAM2005V3r2_global_H_TI_RICE_I.tif 
     * Pls be noticed that the multiple cropping has been considered as the harvest area from SPAM2005 counted this part
+
+2 - Calculate how much irrigation water amount will be distributed to the main crops
+    Irrigation_Amount_to_MainCrop = Frac_MainCrop * Monthly irrigation amount from Bram
+    Here, we:
+    2-1: Sum up the irrigation amount from all sectors (groundwater, surface water, dam, etc.) # Unit: mm
+         [OUT_WI_COMP_SECT,OUT_WI_DAM_SECT,OUT_WI_GW_SECT,OUT_WI_NREN_SECT,OUT_WI_REM_SECT,OUT_WI_SURF_SECT]
+         * Pls be noticed that here the mm is calculated using the total irrigation amount/pixel area
+    2-2: Check if the grids match or not
+    2-3: Calculate the monthly irrigation amount goes to main crop
+
+3 - Calculate the 
